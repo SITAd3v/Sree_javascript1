ae5b1ce7c28fd4fdfeb9c37fed90b8f7e46de678
Sree Peri <jyothi_sarma@yahoo.com>
2024-01-03 15:12:29 +0100

    third day work

---
+++ day3.js
@@ -0,0 +1,34 @@
+var a = 'This is a string.' + 'It is a joint string.';
+console.log(a);
+
+var b = 'We are from SITA.';
+var c =  b + ' We are learning Js.';
+console.log(c);
+
+var d =" This is a string.";
+d += ' It is a joint string.';
+console.log(d);
+
+var x = ' awesome.';
+var y = 'SITA is';
+y +=x;
+console.log(y);
+
+// length of a word
+var firstnamelength = 0;
+var firstname = 'jyothi';
+firstnamelength = firstname.length;
+console.log(firstname);
+
+var firstname = "jyothi";
+firstLetterOfFirstName = firstname [1];
+console.log(firstLetterOfFirstName);
+
+var lastname = 'peri';
+var lastletteroflastname = lastname[lastname.length-3];
+console.log(lastletteroflastname);
+
+
+
+
+
--- decremental.js
+++ decremental.js
@@ -1,6 +1,3 @@
 var x =10;
 x --;
 console.log(x);
-var y = 10;
-z = y + y --;
-console.log(z);
